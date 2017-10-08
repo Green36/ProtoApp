15e75d5d7b566a460e43c7a1951ab1f94b0ba5aa
pj <p@j>
2017-10-09 01:14:30 +0900

    first commit.

---
+++ app/src/main/java/jp/k/green/protoapp/view/ProtoActivity.java
@@ -0,0 +1,15 @@
+package jp.k.green.protoapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jp.k.green.protoapp.R;
+
+public class ProtoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proto);
+    }
+}
