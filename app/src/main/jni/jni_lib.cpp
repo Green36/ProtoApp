83b842bc0cf95b19e8f0632d343f371ff53c7609
pj <p@j>
2017-10-18 02:48:52 +0900

    add *.cpp and *.h file.

--- app/src/main/jni/jni_lib.cpp
+++ app/src/main/jni/jni_lib.cpp
@@ -1,10 +1,11 @@
 #include <jni.h>
 #include <string>
+#include "sample.h"
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_jp_k_green_protoapp_domain_JniWrapper_stringFromJNI(
         JNIEnv *env, jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++" + sample();
     return env->NewStringUTF(hello.c_str());
 }
