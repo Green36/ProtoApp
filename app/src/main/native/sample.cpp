866b3228da2cc034cc09d19cc988ced84f312f67
pj <p@j>
2017-10-18 02:48:52 +0900

    add *.cpp and *.h file.

---
+++ app/src/main/native/sample.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+#include <sstream>
+#include "sample.h"
+
+std::string sample(){
+    return "aaa";
+}
---
+++ app/src/main/native/sample.h
@@ -0,0 +1,9 @@
+#ifndef PROTOAPP_SAPMLE_H
+#define PROTOAPP_SAPMLE_H
+
+#include <bits/basic_string.h>
+
+std::string sample();
+
+
+#endif //PROTOAPP_SAPMLE_H
